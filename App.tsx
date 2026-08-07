8b0cff954dfbd6c5392244fb1618bbea0154cd81
MetalHeadW <mateusdwendhausen@gmail.com>
2026-08-06 21:39:16 -0300

    Fazendo umas coisinhas ai

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const [selecionado, setSelecionado] = useState<boolean>(false);
+return (
+<View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+<View style={{ width: 50, height: 50, backgroundColor: 'red' }}></View>
+<View style={{ width: 50, height: 50, backgroundColor: 'green' }}></View>
+<View style={{ width: 50, height: 50, backgroundColor: 'blue' }}></View>
+</View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+}
+const styles = StyleSheet.create({texto: {
+fontFamily: 'System',
+marginBottom: 15,
+padding: 5,
+},
+titulo: {
+fontSize: 24
+},
+textoDestacado: {
+color: '#0066CC',
+fontWeight: 'bold',
+backgroundColor: '#E6F0FF',
+},
+botao: {
+backgroundColor: '#4CAF50',
+padding: 10,
+borderRadius: 5,
+alignItems: 'center',
+marginTop: 10,
+}
 });
+
+
